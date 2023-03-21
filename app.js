203d2b8648a08179789253a70a478a31dc99cc2e
Abner <contato.abnerbolandim@gmail.com>
2023-03-21 11:22:39 -0300

    Adiciona função para remoção de despesas

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ class Bd {
 				continue;
 			}
 
+			despesa.id = i;
 			despesas.push(despesa);
 		}
 
@@ -94,6 +95,12 @@ class Bd {
 				(d) => d.valor == despesa.valor,
 			);
 		}
+
+		return despesasFiltradas;
+	}
+
+	remover(id) {
+		localStorage.removeItem();
 	}
 }
 
@@ -155,7 +162,65 @@ function carregaListaDespesas() {
 
 	despesas = bd.recuperarTodosRegistros();
 
-	var listaDespesas = document.getElementById("listaDespesas");
+	let listaDespesas = document.getElementById("listaDespesas");
+	listaDespesas.innerHTML = "";
+
+	despesas.forEach(function (d) {
+		let linha = listaDespesas.insertRow();
+
+		linha.insertCell(0).innerHTML = "${d.dia}/${d.mes}/${d.ano}";
+
+		switch (d.tipo) {
+			case "1":
+				d.tipo = "Alimentação";
+				break;
+			case "2":
+				d.tipo = "Educação";
+				break;
+			case "3":
+				d.tipo = "Lazer";
+				break;
+			case "4":
+				d.tipo = "Saúde";
+				break;
+			case "5":
+				d.tipo = "Transporte";
+				break;
+		}
+
+		linha.insertCell(1).innerHTML = d.tipo;
+
+		linha.insertCell(2).innerHTML = d.descricao;
+		linha.insertCell(3).innerHTML = d.valor;
+
+		let btn = document.createElement("button");
+		btn.className = "btn btn-danger";
+		btn.innerHTML = '<i class="fas fa-times"></i>';
+		btn.id = `id_despesa_${d.id}`;
+		btn.onclick = function () {
+			let id = this.id.replace("id_despesa_", "");
+
+			bd.remover(id);
+
+			window.location.reload();
+		};
+		linha.insertCell(4).append(btn);
+	});
+}
+
+function pesquisarDespesa() {
+	let ano = document.getElementById("ano").value;
+	let mes = document.getElementById("mes").value;
+	let dia = document.getElementById("dia").value;
+	let tipo = document.getElementById("tipo").value;
+	let descricao = document.getElementById("descricao").value;
+	let valor = document.getElementById("valor").value;
+
+	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
+
+	let despesas = bd.pesquisar(despesa);
+
+	let listaDespesas = document.getElementById("listaDespesas");
 
 	despesas.forEach(function (d) {
 		let linha = listaDespesas.insertRow();
@@ -185,17 +250,4 @@ function carregaListaDespesas() {
 		linha.insertCell(2).innerHTML = d.descricao;
 		linha.insertCell(3).innerHTML = d.valor;
 	});
-}
-
-function pesquisarDespesa() {
-	let ano = document.getElementById("ano").value;
-	let mes = document.getElementById("mes").value;
-	let dia = document.getElementById("dia").value;
-	let tipo = document.getElementById("tipo").value;
-	let descricao = document.getElementById("descricao").value;
-	let valor = document.getElementById("valor").value;
-
-	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
-
-	bd.pesquisar(despesa);
 }
